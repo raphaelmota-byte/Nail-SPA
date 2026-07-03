6896b156c830c49ad0ab01c8b7c052ed1a9fb9d4
raphaelmota-byte <raphamota08@gmail.com>
2026-07-03 19:12:59 -0300

    galeria adicionada - classes css e keyframes adicionadas - img/galeria adicionada - dicionario com url e alt para img/funcionando corretamente

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -5,9 +5,21 @@ def home(request):
     servicos = Servico.objects.filter(ativo=True)
     config = ConfiguracaoSite.objects.first()
     
+    dicionario_galeria_alt = {
+    "agendamentos/img/galeria/alongamento.jpg": "alongamento",
+    "agendamentos/img/galeria/pe.avif": "pe",
+    "agendamentos/img/galeria/ambiente_sofisticado.png": "ambiente sofisticado",
+    "agendamentos/img/galeria/bioseguranca.png": "biosegurança",
+    "agendamentos/img/galeria/foto_servico_manicure.jfif": "manicure",
+    "agendamentos/img/galeria/foto_unha_azul.jpg": "unha azul",
+    "agendamentos/img/galeria/produtos_premium.jfif": "produtos premium"
+        
+    }
+    
     
     return render(request , "agendamentos/home.html" , {
         "servicos" : servicos ,
-        "config" : config
+        "config" : config ,
+        "dic_galeria_alt" : dicionario_galeria_alt
     })
 
